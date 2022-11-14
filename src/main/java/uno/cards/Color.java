ce1fcff149fe02ae7957431ab2524d457c65d7c5
Seekard <maks_byx@mail.ru>
2022-11-14 18:23:39 +0300

    Get color by index were added

--- src/main/java/uno/cards/Color.java
+++ src/main/java/uno/cards/Color.java
@@ -1,10 +1,17 @@
 package uno.cards;
 
+import java.util.Arrays;
+
 public enum Color
 {
     Red,
     Green,
     Blue,
     Yellow,
-    Any
+    Any;
+
+    public static Color getByIndex(int index)
+    {
+        return Arrays.stream(values()).toList().get(index);
+    }
 }
